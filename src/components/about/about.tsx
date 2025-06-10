cbcc67dc5bba4e68ea0cd93601577d78f98c1635
mahamadou <mahamadou.gory.sio@gmail.com>
2025-06-10 10:43:33 +0200

    ✨ feat(about): Add initial structure and content for About component

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -8,11 +8,14 @@ export default function about() {
             <div>
                 {/*caroussel with image about the company */}
                 <div>
-
+                    
                 </div>
                 {/*text of presentation of the company */}
                 <div>
-
+                    <h3>About maisoon horifique</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore veritatis veniam iusto nemo optio. Libero voluptates asperiores, nihil ullam ea aperiam reiciendis. Sit reprehenderit doloremque dolorum blanditiis dolorem rem numquam.
+                    </p>
                 </div>
                 
             </div>
