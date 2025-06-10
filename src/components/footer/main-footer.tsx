ac4bd95b500fd197f647ce01b2a1997e81f9c1b9
mahamadou <mahamadou.gory.sio@gmail.com>
2025-06-10 10:15:33 +0200

    Add main footer component structure

---
+++ src/components/footer/main-footer.tsx
@@ -0,0 +1,26 @@
+
+
+export default function mainFooter() {
+    return (
+        <>
+            <footer>
+                {/*social media section */}
+                <div>
+                
+                </div>
+                {/*detail session */}
+                <div>
+
+                </div>
+                {/*Contact form */}
+                <div>
+
+                </div>
+                {/*Copyright*/}
+                <div>
+
+                </div>
+            </footer>
+        </>
+    )
+}
