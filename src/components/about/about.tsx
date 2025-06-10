a4e62c81b52b35fbec94d51d1823f3e7fc6970ec
mahamadou <mahamadou.gory.sio@gmail.com>
2025-06-10 10:36:26 +0200

    Add About component with initial structure and placeholders for content

---
+++ src/components/about/about.tsx
@@ -0,0 +1,21 @@
+
+
+export default function about() {
+
+    return (
+        <>
+            {/*Main Bloc */}
+            <div>
+                {/*caroussel with image about the company */}
+                <div>
+
+                </div>
+                {/*text of presentation of the company */}
+                <div>
+
+                </div>
+                
+            </div>
+        </>
+    )
+}
