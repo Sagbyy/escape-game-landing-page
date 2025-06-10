c3dde748dd8e857f12ba2406522a13c9baed2d57
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-06-10 11:12:27 +0200

    ✨ feat(App): Wrap application in ThemeProvider and include Introduction component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import MainFooter from "./components/footer/main-footer";
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/navbar";
+import Introduction from "./components/introduction";
+import { ThemeProvider } from "./components/utils/theme-provider";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Navbar />
-      <MainFooter/>
-    </>
+      <Introduction />
+      <MainFooter />
+    </ThemeProvider>
   );
 }
 
