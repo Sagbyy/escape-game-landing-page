01dc2ac5d07f6be142dbba1d83591a441feaf7a5
mahamadou <mahamadou.gory.sio@gmail.com>
2025-07-06 21:21:57 +0200

    ✨ fix(vite.config): Update base path for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -11,5 +13,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base : "/",
+  base : isProduction ? "/escape-game-landing-page/" : "/",
 });
