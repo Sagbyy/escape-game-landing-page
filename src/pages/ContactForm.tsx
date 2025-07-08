10e76235f2f88a58e31f1c2fcd2fc604fd4cd0cc
mahamadou <mahamadou.gory.sio@gmail.com>
2025-07-08 10:33:13 +0200

    ✨ feat(contact): implement ContactForm component with basic structure

---
+++ src/pages/ContactForm.tsx
@@ -0,0 +1,9 @@
+export default function ContactForm(){
+    
+    return(
+        <div className="p-6">
+            <h1 className="text-2xl text-red-500 mb-4"> Contactez-nous</h1>
+            <ContactForm />
+        </div>
+    )
+}
