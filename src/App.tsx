3989e3d34abce9343c6b18c6559239906ccd0d01
mahamadou <mahamadou.gory.sio@gmail.com>
2025-07-08 10:49:22 +0200

    ✨ feat(app): refactor App component to use Routes for MainContent and ContactForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import About from "./components/about/about";
 import MainFooter from "./components/footer/main-footer";
-import Introduction from "./components/introduction";
 import Navbar from "./components/Navbar";
-import SessionsPreview from "./components/sessions-preview";
 import { ThemeProvider } from "./components/utils/theme-provider";
+import { Route, Routes } from "react-router";
+import MainContent from "./pages/MainContent";
+import ContactForm from "./components/contact/contactForm";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Navbar />
-      <Introduction />
-      <About />
-      <SessionsPreview />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/contact" element={<ContactForm />} />
+      </Routes>
       <MainFooter />
     </ThemeProvider>
   );
