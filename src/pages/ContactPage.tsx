0bba517991210cf1e215c906597c86db88464387
mahamadou <mahamadou.gory.sio@gmail.com>
2025-07-08 11:27:37 +0200

    ✨ feat(pages): update padding in ContactPage and LoginPage components

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -3,8 +3,7 @@ import ContactForm from "@/components/contact/contactForm";
 export default function ContactPage(){
     
     return(
-        <div className="p-6">
-            <h1 className="text-2xl text-red-500 mb-4"> Contactez-nous</h1>
+        <div className="p-40">
             <ContactForm />
         </div>
     )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,7 @@ import LoginForm from "@/components/login/loginForm";
 
 export default function LoginPage() {
     return (
-        <div className="p-6">
-            <h1 className="text-2xl text-red-500 mb-4">Se connecter</h1>
+        <div className="p-40">
             <LoginForm />
         </div>
     )
